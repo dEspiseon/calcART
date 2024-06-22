703fcaa0c14c62b6cbee21473493fe2ca79e9f3a
Bezgin <SergeyCorb@yandex.ru>
2024-06-22 10:46:43 +0300

    no message

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 80;
+const host = '0.0.0.0';
 
 // Настройка статической папки
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Запуск сервера
-app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
+app.listen(port, host, () => {
+    console.log(`Сервер запущен на http://${host}:${port}`);
 });
